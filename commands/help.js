ad016e406f5b6dfaf20ee335de0b609d76d4aaed
avery <fruitcocktail0408@gmail.com>
2020-05-27 20:52:57 +0100

    help updated

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'help',
 	description: 'Help',
 	execute(message) {
-		message.channel.send('I\'m ChrisBot, here to help. If you want a list of my useless commands, go to fruitcktail.github.io/ChrisBot. This is ChrisBot v1.0');
+		message.channel.send('I\'m ChrisBot, here to help. If you want a list of my useless commands, go to fruitcktail.github.io/ChrisBot. This is ChrisBot v1.0.1');
         console.log(`Help given.`);
 	},
 };
