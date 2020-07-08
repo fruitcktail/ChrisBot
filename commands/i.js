db474da7b6bcce001bc3ac6acd86309e1f96f925
avery <fruitcocktail0408@gmail.com>
2020-07-08 18:14:30 +0100

    aaa

--- commands/i.js
+++ commands/i.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'i',
     description: 'love/hate relationship',
 	execute(message, args) {
-        if (args[0] === "love") {
+        if (args[0].toLowerCase() === "love") {
             if (args[1] == "you") {
                 message.reply("I love you too!");
             }
